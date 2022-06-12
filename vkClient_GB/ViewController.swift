f23ea08a381c32a65376cb70d6385f16471d0ca2
skokdmitriy <skokdmitriy@gmail.com>
2022-06-12 14:54:21 +0700

    Initial Commit

---
+++ vkClient_GB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vkClient_GB
+//
+//  Created by Дмитрий Скок on 12.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
